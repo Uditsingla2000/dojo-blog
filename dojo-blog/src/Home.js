d435d56a71e653698913da653b2ebf0219262415
Udit Singla <uditsingla2000@gmail.com>
2024-05-13 00:36:01 +0530

    adding bloglist component

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import {useState} from "react";
+import BlogList from './BlogList'
 
 export default function Home() {
 
-  const [name,setName] = useState('mario');
-  const [age,setAge] = useState(20);
-
-  const handleClick = () => {
-    setName('luigi');
-    setAge(30);
-  }
-
+  const [blogs,setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
   return (
     <div className="Home">
-      <h1>HomePage</h1>
-      <p>{name} is {age} old</p>
-      <button type="button" onClick={handleClick}>Click me</button>
+      <BlogList blogs={blogs} title= "All blogs" ></BlogList>
+      {/* <BlogList blogs={blogs.filter((blog)=> blog.author==='mario')} title= "Mario's blogs" ></BlogList> */}
+
     </div>
   )
 }
